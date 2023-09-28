586c01a4a1914569547889397c38384e75d27a39
Tarikul69 <tarikulabir69@gmail.com>
2023-09-29 00:38:05 +0600

    Message

---
+++ lib/chat.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class char extends StatelessWidget {
+  const char({super.key});
+
+  Widget mycard(){
+    return  Container(
+        child: InkWell(
+          onTap: (){
+            Get.to("page");
+          },
+          child: Card(
+            //color: Colors.grey,
+            child: Row(
+              children: [
+                const SizedBox(height: 10,),
+                Container(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('jrhughiuherg/ijirjf/iji'),
+              ),
+            ),
+            SizedBox(width: 200,),
+            Row(
+              children: [
+            Text("data"),
+              ],
+            ) 
+              ],
+            ),
+          ),
+        ),
+    );
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            mycard(),
+            mycard(),
+            mycard(),
+            mycard(),
+            mycard(),
+            mycard(),
+            mycard(),
+            mycard(),
+            mycard(),
+            mycard(),
+            mycard(),
+            mycard(),          
+            mycard(),
+            mycard(),
+            mycard(),
+            mycard(),          
+            mycard(),
+            mycard(),
+            mycard(),
+            mycard(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/model/usersmodel.dart
@@ -0,0 +1,11 @@
+class usersmodel {
+  
+  String? uid;
+  String? fullname;
+  String? email;
+  String? profile;
+
+  usersmodel({this.uid, this.fullname, this.email, this.profile});
+
+  
+}
